fff8d0155de281f729efa6b24369bd52f7b96691
kareem1207 <kareemshaik1207@gmail.com>
2025-01-06 20:34:17 +0530

    added copy feature

--- src/Ui/Text.jsx
+++ src/Ui/Text.jsx
@@ -2,13 +2,37 @@ import { ButtonCaps } from "@/components/ButtonCaps.jsx";
 import { ButtonSmall } from "@/components/ButtonSmall.jsx";
 import { ButtonNum } from "@/components/ButtonNum";
 import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastsify/dist/ReactToastify.css';
+import { MdContentCopy } from "react-icons/md";
 
 export const Text = ()=>{
     const [text , setText] = useState("");
 
+    const copyToClickBoard = async () => {
+        try {
+            await navigator.clipboard.writeText(text);
+            toast.success('Successfully copied! 🎉', {
+                position: "bottom-right",
+                autoClose: 2000,
+                icon: "🚀",
+                style: {
+                    background: '#4CAF50',
+                    color: 'white',
+                    borderRadius: '10px',
+                    boxShadow: '0 4px 12px rgba(0,0,0,0.15)'
+                }
+            });
+        } catch (err) {
+            toast.error('Failed to copy! 😢\nFollowing Error occurred : '+err);
+        }
+    }
+
     return <>
         <h1>Text</h1>
         <p>{text}</p>
+        <button onClick={copyToClickBoard}><MdContentCopy /></button>
+        <ToastContainer />
         <ButtonNum displayText ={setText} display={text} />
         <ButtonCaps displayText ={setText} display={text} />
         <ButtonSmall displayText ={setText} display={text} />
