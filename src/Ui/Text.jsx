8ee80b0fdd0f5337299da245af98c3efba0375e8
kareem1207 <kareemshaik1207@gmail.com>
2025-01-06 19:34:29 +0530

    updated ButtonCaps by adding multikey press configuration

--- src/Ui/Text.jsx
+++ src/Ui/Text.jsx
@@ -1,10 +1,10 @@
 import { ButtonCaps } from "@/components/ButtonCaps.jsx";
 import { useState } from "react";
 export const Text = ()=>{
-    const [text , setText] = useState("hi ");
+    const [text , setText] = useState("");
     return <>
         <h1>Text</h1>
         <p>{text}</p>
-        <ButtonCaps displayText ={setText} />
+        <ButtonCaps displayText ={setText} text={text} />
     </>
 }
--- src/components/ButtonCaps.jsx
+++ src/components/ButtonCaps.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
+
 /* eslint-disable react/prop-types */
 export const ButtonCaps = ({displayText})=>{
+    const [display , setDisplay]=useState("");
     
-    const handleDisplay = ()=>{ 
-        displayText("A");
-    }
-
-    const handleDisplayB = ()=>{ 
-        displayText("B");
+    const handleDisplay = (text)=>{ 
+        const newDisplay = display + text;
+        setDisplay(newDisplay);
+        console.log(display);
+        displayText(newDisplay);
     }
 
     return <>
-        <button onClick ={handleDisplay}>A</button>
-        <button onClick ={handleDisplayB}>B</button>
+        <button onClick ={()=>handleDisplay("A")}>A</button>
+        <button onClick ={()=>handleDisplay("B")}>B</button>
+
     </>
 }
